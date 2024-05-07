daf2682045907e487b8d3025aa5c3fa08877ebca
hunterfaugot <hbf9768@gmail.com>
2024-05-06 19:23:50 -0500

    fixed issues and answered questions

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 document.getElementById("fetch-user").addEventListener("click", function () {
-  fetch("https://randomuser.me/apii/") // Question 1: Why does this fetch call fail?
+  fetch("https://randomuser.me/api/") // Question 1: Why does this fetch call fail?
+    .then((response) => response.json())
     .then((data) => {
       displayUser(data.results[0]); // Question 2: Why is data.results undefined?
     })
@@ -9,11 +10,11 @@ document.getElementById("fetch-user").addEventListener("click", function () {
 function displayUser(user) {
   const userInfoDiv = document.getElementById("user-info");
   // Question 3: Why isn't the user's name displaying correctly?
-  userInfoDiv.innerHTML = `Name: ${user.first} ${user.name}<br>
+  userInfoDiv.innerHTML = `Name: ${user.name.first} ${user.name.last}<br>
                            Email: ${user.email}`;
 }
 
-// Question 4: Why does this API call fail?
+// Question 4: Why does this API call fail?  base URL is wrong
 fetch("https://api.example.com/data")
   .then((response) => response.json())
   .catch((error) => {
@@ -29,7 +30,7 @@ function fetchNews() {
       // Question 5: Why do the article titles not appear on the screen?
       articles.forEach((article) => {
         const p = document.createElement("p");
-        p.textContent = article.name;
+        p.textContent = article.title;
         container.appendChild(p);
       });
     })
